2197831c93a12d77800977ef26c486b576a9bf43
Nadiaere <knw_@list.ru>
2022-11-23 18:46:51 +0500

    Exsample02

---
+++ Exsample/Exsample02/Program.cs
@@ -0,0 +1,13 @@
+﻿// UserName
+
+Console.WriteLine("Введите ваше имя");
+string? username = Console.ReadLine();
+Console.WriteLine("Привет");
+Console.WriteLine(username);
+
+// UserName2
+
+Console.Write("Введите ваше имя2");
+string? username2 = Console.ReadLine();
+Console.Write("Привет");
+Console.Write(username2);
