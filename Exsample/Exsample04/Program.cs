5362b1cb168ac8d8284ac0d0272ba5119c61e828
Nadiaere <knw_@list.ru>
2022-11-23 19:46:43 +0500

    Exsample04

---
+++ Exsample/Exsample04/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вычисление частного двух чисел
+// Div - деление
+// Пример 1 целое число
+
+int numberA = 10;
+int numberB = 5;
+Console.WriteLine(numberA / numberB);
+
+// пример 2  вещественное число
+
+double numberC = 12;
+double numberD = 5;
+Console.WriteLine(numberC / numberD);
