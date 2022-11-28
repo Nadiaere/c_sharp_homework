ed792c8e0d1016f85e41bd940c53edaf6049b7d5
Nadiaere <knw_@list.ru>
2022-11-28 21:44:43 +0500

    Exsample012 Void

---
+++ Exsample/Exsample012/Program.cs
@@ -0,0 +1,33 @@
+﻿// Массивы.
+// void метод который ничего не возвращает
+// FillArray(out int[] arr), функция, которая создает и заполняет новый массив C#
+
+void FillArray(int[] collection)     // collection - аргумент
+{
+    int Lenght = collection.Length;
+    int index = 0;
+    while (index < Lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)                              //  Метод печати
+{
+    int count = col.Length;    // количество элементов
+    int Position = 0;
+    while (Position < count)
+    {
+        Console.WriteLine(col[Position]);     // вывод текущей позиции элемента
+        Position++;
+    }
+
+}
+int [] array = new int [10];
+FillArray(array);   // метод который заполнил массив
+PrintArray(array);   // метод печати
+
+
+
+
+ 
