0d0808e8a972bfee731d538ac941da426d78d808
Nadiaere <knw_@list.ru>
2022-12-27 21:02:29 +0500

    Задача 66. Сумма натуральных элементов в промежутке

---
+++ HomeTasks/Lesson09/Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int CountNaturalSum(int M, int N)
+{
+    if (M == N)
+        return N;
+    return N + CountNaturalSum(M, N - 1);
+}
+
+Console.WriteLine($"\n Сумма натуральных элементов в промежутке от {M} до {N} = {CountNaturalSum(M, N)}");
